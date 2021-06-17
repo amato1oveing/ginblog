90afacfbd435efbbd4ad6619c0779bee9698c577
zc <2164951481@qq.com>
2021-06-17 22:39:24 +0800

    add

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module ginblog
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	gopkg.in/ini.v1 v1.62.0
+)
---
+++ routes/router.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"ginblog/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	gin.SetMode(utils.AppMode)
+	r := gin.Default()
+}
